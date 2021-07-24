0e40f556c0413c81d97ced06a11c49d10cd83588
francomachado21 <historiasdeunsapoql@hotmail.com>
2021-07-24 03:42:40 -0300

    interfaz de registro e ingreso

---
+++ Programacion/Frontend/Clases Comunes/Interface_Registro.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">	 	 
+ 	<head>
+     <style>
+       h2 {text-align:center; font-family: "Microsoft JhengHei";}  
+       form{margin: auto; width: 50%; max-width: 360px; background: #EEEEEE; padding: 20px;}
+       input{display: block; padding: 10px; width: 92%; margin: 30px 0; font-size: 20px; margin-top: 0px;}
+       input[type="submit"] {background: linear-gradient(#734593,#D25039); font-family: "Microsoft JhengHei"; cursor: pointer; border-radius: 20px; margin-bottom: 0px; opacity: 0.8;}
+       input[type="submit"]:hover {opacity: 1;}
+       input[type="submit"]:active {transform: scale(0.95)}
+       @media (max-width: 768px){form {width: 75% }}
+       @media (max-width: 480px){form {width: 90% }}
+</style>	 
+ 	 	<TITLE>Registro de Alumno</TITLE>
+    </head>	 
+ 	<BODY>	 
+         <h2>Registro de Alumno</h2>
+ 	 	<form action"">
+              
+              <input type="text" placeholder="Cedula" name="cedula" >
+              <input type="text" placeholder="Nombre Nombre" name="nombrealumno" >
+              <input type="text" placeholder="Apellido Apellido" name="apellidoalumno" >
+              <input type="text" placeholder="Grupo" name="grupoalumno" >
+              <input type="password" placeholder="Contraseña" name="passalumno" >
+              <input type="password" placeholder="Verificar Contraseña" name="passveralumno" >
+              <input type="submit" value="Registrarse" style="margin: 3%;"> 
+ 	</BODY>	 
+</HTML>	 	 
