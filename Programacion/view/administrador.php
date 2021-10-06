245d4397e9496ef48c3b8a914302643651c0c21d
BrunCoA <brunocorreaaltez2016@gmail.com>
2021-10-06 18:54:02 -0300

    primera bruno 6/10

--- Programacion/view/administrador.php
+++ Programacion/view/administrador.php
@@ -5,23 +5,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Página principal del administrador</title>
-<style>
-    header{
-        background-color: brown;
-    }
-</style>
-
+    <link rel="stylesheet" href="css/appadmin.css">
 </head>
 <body>
     <div class="container-all">
         <header>
-            <div>
-                <div>
-                    <img src="chatmate_logo.png" alt="ChatMate">
+            <div class="nav">
+                <div class="nav1">
+                   <a href="">
+                       <img src="css/ChatMate.svg" alt="ChatMate">
+                   </a>
                 </div>
-                <div>
+                <div class="nav2">
                     <a href="">
-                            <img src="mensaje.png" alt="Solicitudes">
+                    <img src="css/solicitud.svg" alt="Solicitud Alumno">
                     </a>
                 </div>
 
