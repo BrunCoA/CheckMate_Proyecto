acfb7fe8982a4ceb4625935c47062dac4f7e4840
BrunCoA <brunocorreaaltez2016@gmail.com>
2021-11-05 19:25:33 -0300

    quinta de bruno 5/11

--- Programacion/model/db_chatmate.sql
+++ Programacion/model/db_chatmate.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 14-09-2021 a las 02:36:53
+-- Tiempo de generación: 05-11-2021 a las 23:04:04
 -- Versión del servidor: 10.4.20-MariaDB
 -- Versión de PHP: 8.0.9
 
@@ -31,6 +31,13 @@ CREATE TABLE `administrador` (
   `ci` int(8) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Volcado de datos para la tabla `administrador`
+--
+
+INSERT INTO `administrador` (`ci`) VALUES
+(11111111);
+
 -- --------------------------------------------------------
 
 --
@@ -54,8 +61,18 @@ CREATE TABLE `agenda` (
 
 CREATE TABLE `alumno` (
   `ci` int(8) NOT NULL,
-  `apodo` varchar(25) NOT NULL
+  `apodo` varchar(25) NOT NULL,
+  `foto` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `alumno`
+--
+
+INSERT INTO `alumno` (`ci`, `apodo`, `foto`) VALUES
+(22222222, 'joaquino', ''),
+(51823293, 'bcorrea', ''),
+(51800449, 'frankymachado', '');
 
 -- --------------------------------------------------------
 
@@ -65,8 +82,17 @@ CREATE TABLE `alumno` (
 
 CREATE TABLE `alumno_ingresa_grupo` (
   `ci` int(8) NOT NULL,
-  `id_gr` varchar(5) NOT NULL
+  `id_gr` int(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `alumno_ingresa_grupo`
+--
+
+INSERT INTO `alumno_ingresa_grupo` (`ci`, `id_gr`) VALUES
+(22222222, 2),
+(51823293, 9),
+(51800449, 9);
 
 -- --------------------------------------------------------
 
@@ -80,6 +106,23 @@ CREATE TABLE `asignatura` (
   `id_or` int(36) NOT NULL,
   `nom_asig` varchar(64) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `asignatura`
+--
+
+INSERT INTO `asignatura` (`id_asig`, `tipo`, `id_or`, `nom_asig`) VALUES
+(1, 1, 2, 'matematica'),
+(2, 1, 3, 'matematica'),
+(3, 1, 4, 'matematica'),
+(4, 1, 5, 'matematica'),
+(5, 1, 6, 'matematica'),
+(6, 1, 1, 'biologia'),
+(7, 0, 2, 'programacion web'),
+(8, 0, 2, 'diseño web'),
+(9, 1, 3, 'filosofia'),
+(10, 0, 5, 'redes'),
+(11, 1, 6, 'quimica');
 
 -- --------------------------------------------------------
 
@@ -103,8 +146,17 @@ CREATE TABLE `consulta` (
 --
 
 CREATE TABLE `docente` (
-  `ci` int(8) NOT NULL
+  `ci` int(8) NOT NULL,
+  `foto` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `docente`
+--
+
+INSERT INTO `docente` (`ci`, `foto`) VALUES
+(33333333, ''),
+(44444444, '');
 
 -- --------------------------------------------------------
 
@@ -138,9 +190,31 @@ CREATE TABLE `entra_sala` (
 --
 
 CREATE TABLE `grupo` (
-  `id_gr` varchar(5) NOT NULL,
+  `id_gr` int(4) NOT NULL,
+  `nom_gr` varchar(5) NOT NULL,
   `id_or` int(36) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `grupo`
+--
+
+INSERT INTO `grupo` (`id_gr`, `nom_gr`, `id_or`) VALUES
+(1, '3ºBA', 1),
+(2, '3ºBB', 1),
+(3, '3ºBC', 1),
+(4, '3ºBE', 1),
+(5, '3ºBF', 1),
+(6, '3ºBA', 2),
+(7, '3ºBB', 2),
+(8, '3ºBC', 2),
+(9, '3ºBE', 2),
+(10, '3ºBF', 2),
+(11, '3ºBA', 3),
+(12, '3ºBB', 3),
+(13, '3ºBC', 4),
+(14, '3ºBE', 5),
+(15, '3ºBF', 6);
 
 -- --------------------------------------------------------
 
@@ -151,7 +225,9 @@ CREATE TABLE `grupo` (
 CREATE TABLE `mensaje` (
   `id_mensaje` int(36) NOT NULL,
   `id_chat` int(36) NOT NULL,
-  `ci` int(8) NOT NULL
+  `ci` int(8) NOT NULL,
+  `msj` mediumblob DEFAULT NULL,
+  `created_on` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -177,6 +253,18 @@ CREATE TABLE `orientacion` (
   `nom_or` varchar(64) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Volcado de datos para la tabla `orientacion`
+--
+
+INSERT INTO `orientacion` (`id_or`, `nom_or`) VALUES
+(1, 'biologico'),
+(2, 'desarrollo web'),
+(3, 'artistico'),
+(4, 'videojuegos'),
+(5, 'desarrollo y soporte'),
+(6, 'cientifico');
+
 -- --------------------------------------------------------
 
 --
@@ -199,15 +287,28 @@ CREATE TABLE `sala_chat` (
 CREATE TABLE `usuario` (
   `ci` int(8) NOT NULL,
   `p_nom` varchar(64) NOT NULL,
-  `s_nom` varchar(64) ,
+  `s_nom` varchar(64) NOT NULL,
   `p_ape` varchar(64) NOT NULL,
-  `s_ape` varchar(64) ,
-  `activo` tinyint(1) NOT NULL,
+  `s_ape` varchar(64) NOT NULL,
+  `activo` tinyint(1) NOT NULL DEFAULT 0,
   `pass` varchar(64) NOT NULL,
-  `foto` varchar(255) NOT NULL,
   `hora_conex` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `hora_des` timestamp NULL DEFAULT NULL
+  `hora_des` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`ci`, `p_nom`, `s_nom`, `p_ape`, `s_ape`, `activo`, `pass`, `hora_conex`, `hora_des`) VALUES
+(11111111, 'admin', 'istrador', 'admin', 'istrator', 0, 'admin123321nimda', '2021-11-05 21:54:19', '2021-11-05 21:54:19'),
+(22222222, 'joaquin', 'belando', 'martinez', 'rodriguez', 0, 'a1a1a1a1', '2021-11-05 21:54:19', '2021-11-05 21:54:19'),
+(33333333, 'francisco', 'daniel', 'martinez', 'alvarado', 0, '122112', '2021-11-05 21:54:19', '2021-11-05 21:54:19'),
+(44444444, 'jhin', 'jhin', 'jhin', 'jhin', 0, 'sublime', '2021-11-05 21:54:19', '2021-11-05 21:54:19'),
+(51800449, 'franco', 'joaquin', 'machado', 'rosales', 0, 'franky1432ma4', '2021-11-05 21:54:19', '2021-11-05 21:54:19'),
+(51823293, 'bruno', '', 'correa', 'altez', 0, 'n2ajhe532ndwui', '2021-11-05 21:54:19', '2021-11-05 21:54:19'),
+(55555555, 'brian', '', 'alvarez', 'falero', 0, '55123', '2021-11-05 21:54:19', '2021-11-05 21:54:19'),
+(66666666, 'juan', 'pablo', 'machado', 'rosales', 0, 'aa344o34hf', '2021-11-05 21:54:19', '2021-11-05 21:54:19');
 
 --
 -- Índices para tablas volcadas
@@ -329,13 +430,19 @@ ALTER TABLE `agenda`
 -- AUTO_INCREMENT de la tabla `asignatura`
 --
 ALTER TABLE `asignatura`
-  MODIFY `id_asig` int(36) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_asig` int(36) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT de la tabla `consulta`
 --
 ALTER TABLE `consulta`
   MODIFY `num_cons` int(36) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `grupo`
+--
+ALTER TABLE `grupo`
+  MODIFY `id_gr` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT de la tabla `mensaje`
@@ -353,7 +460,7 @@ ALTER TABLE `mensaje_asincronico`
 -- AUTO_INCREMENT de la tabla `orientacion`
 --
 ALTER TABLE `orientacion`
-  MODIFY `id_or` int(36) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_or` int(36) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `sala_chat`
